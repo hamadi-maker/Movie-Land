e650441a229600648b89cf9d5da26f5a7ed4440c
hamadi-maker <benmokhtarhamadi@gmail.com>
2023-06-22 15:42:30 +0100

    first commit, Setup Files and Setup The OMDb API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+
+import SearchIcon from './search.svg'
 import './App.css';
 
+
+
+// c39933ac
+
+const API_URL ='http://www.omdbapi.com/?i=tt3896198&apikey=c39933ac'
+
+const movie1 ={
+  "Title": "Amazing Spiderman Syndrome",
+  "Year": "2012",
+  "imdbID": "tt2586634",
+  "Type": "movie",
+  "Poster": "N/A"
+} 
+
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    console.log(data)
+  }
+
+  useEffect(() =>{
+    searchMovies('Spiderman');
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Land</h1>
+      <div className="search">
+        <input type="text" placeholder='Search for movies' value='Superman' onChange={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        
+      </div>
     </div>
   );
-}
+  }
 
 export default App;
